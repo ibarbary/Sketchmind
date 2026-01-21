ad35849eea01fdcdec7474287e3bc89f3e4d1ff8
Ahmed Barbary <aelbarbary120@gmail.com>
2026-01-21 21:45:02 +0200

    chore: update Axios base URL to Vercel server

--- client/src/utils/apiAuth.ts
+++ client/src/utils/apiAuth.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiAuth = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://sketchmind-server.vercel.app/",
   withCredentials: true,
 });
 
